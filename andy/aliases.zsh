e657c038b4cd7a550a98e22870bc220fab03cdd7
Andy Taylor <andytlr@gmail.com>
2013-08-02 11:47:25 +1000

    Added alias.

--- andy/aliases.zsh
+++ andy/aliases.zsh
@@ -1,6 +1,7 @@
 alias andy='cd ~/Dropbox/Websites/andy-taylor'
 alias j='cd ~/Ferocia/johanna'
 alias f='cd ~/Ferocia/fub'
+alias build='./script/cibuild'
 alias jd='cd ~/Ferocia/johanna-design'
 alias ji='cd ~/Ferocia/johanna-ios'
 alias fs='foreman start'
